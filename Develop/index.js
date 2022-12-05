19434a9ec96685447d1ae34f86f0136f7c6089e8
sdanimc <116037382+sdanimc@users.noreply.github.com>
2022-12-05 12:42:18 -0800

    updated package.json, added inquirer and fs in js, started questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,25 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "Proj Title",
+        message: "What is the Title of your Project?",
+        type: "input"
+},
+{
+    name: "Github Username",
+    message: "What is your Github Username?",
+    type: "input"
+},
+{
+    name: "email",
+    message: "What is your Email Address?",
+    type: "input"
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
